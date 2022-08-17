0407c0fc18e3f85260d39b176da80610ce0a22ed
unknown <smpatwatkar2000@gmail.com>
2022-08-17 15:37:48 +0530

    make it better

--- tattle/src/reducer/UseReducer.js
+++ tattle/src/reducer/UseReducer.js
@@ -1,9 +1,9 @@
-export const initialState = { token: localStorage.getItem('jwtoken'),
-isAuthenticated: localStorage.getItem('jwtoken') ? true : false,
-isLoading: false,
-isRegistered: false};
+// export const initialState = { token: localStorage.getItem('jwtoken'),
+// isAuthenticated: localStorage.getItem('jwtoken') ? true : false,
+// isLoading: false,
+// isRegistered: false};
 
-// export const initialState = null;
+export const initialState = null;
 export const reducer = (state, action) => {
     if (action.type === "USER") {
         return action.payload;
