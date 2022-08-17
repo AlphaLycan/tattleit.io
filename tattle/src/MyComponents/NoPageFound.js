9c7a539e94a708b3a519f0bd74a79ecb13a79155
unknown <smpatwatkar2000@gmail.com>
2022-08-17 12:14:17 +0530

    make it better

--- tattle/src/MyComponents/NoPageFound.js
+++ tattle/src/MyComponents/NoPageFound.js
@@ -3,29 +3,29 @@ import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../App';
 
 export default function NoPageFound() {
-  const navigate = useNavigate();
-    const { state, dispatch } = useContext(UserContext);
-   useEffect(() => {
-      fetch('/logout', {
-         method: 'GET',
-         headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-         },
-         credentials: 'include'
-      })
-         .then((res) => {
-            dispatch({ type: 'USER', payload: false });
-            navigate('/SignIn', { replace: true });
-            if (res.status !== 200) {
-               const error = new Error(res.error);
-               throw error;
-            }
-         })
-         .catch((err) => {
-            console.log(err);
-         });
-   }, []);
+//   const navigate = useNavigate();
+//     const { state, dispatch } = useContext(UserContext);
+//    useEffect(() => {
+//       fetch('/logout', {
+//          method: 'GET',
+//          headers: {
+//             Accept: 'application/json',
+//             'Content-Type': 'application/json'
+//          },
+//          credentials: 'include'
+//       })
+//          .then((res) => {
+//             dispatch({ type: 'USER', payload: false });
+//             navigate('/SignIn', { replace: true });
+//             if (res.status !== 200) {
+//                const error = new Error(res.error);
+//                throw error;
+//             }
+//          })
+//          .catch((err) => {
+//             console.log(err);
+//          });
+//    }, []);
 
   return (
     <div><h1>404 NoPageFound</h1>
