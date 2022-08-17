3fd3bcb762c483f7fafbf69caa3f1e260761b584
unknown <smpatwatkar2000@gmail.com>
2022-08-17 20:17:47 +0530

    make it better

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const cookieParser = require('cookie-parser');
+const path = require('path');
+
 app.use(cookieParser());
 dotenv.config({path: './config.env'});
 require('./db/conn');
